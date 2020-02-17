7717355f4e6d8fd839e97b3fce1a0220ee2eb44c
gitanada <58547890+gitanada@users.noreply.github.com>
2020-02-17 19:41:43 +0700

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,38 @@
+{
+  "name": "RelungHati",
+  "short_name": "RelungHati",
+  "start_url": "https://www.relunghati.com/",
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#C51162",
+  "description": "Kumpulan Doa Lengkap dan Mengulas Sejarah Islam.",
+  "icons": [{
+    "src": "https://1.bp.blogspot.com/-PcmCFbLVd48/XkbYGRO0PgI/AAAAAAAAAL8/HZCSnBi5lQQ3Rp2GMgyVnjhMKTr2xQutwCLcBGAsYHQ/s1600/android-icon.png",
+    "sizes": "48x48",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-PcmCFbLVd48/XkbYGRO0PgI/AAAAAAAAAL8/HZCSnBi5lQQ3Rp2GMgyVnjhMKTr2xQutwCLcBGAsYHQ/s1600/android-icon.png",
+    "sizes": "72x72",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-PcmCFbLVd48/XkbYGRO0PgI/AAAAAAAAAL8/HZCSnBi5lQQ3Rp2GMgyVnjhMKTr2xQutwCLcBGAsYHQ/s1600/android-icon.png",
+    "sizes": "96x96",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-PcmCFbLVd48/XkbYGRO0PgI/AAAAAAAAAL8/HZCSnBi5lQQ3Rp2GMgyVnjhMKTr2xQutwCLcBGAsYHQ/s1600/android-icon.png",
+    "sizes": "144x144",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-PcmCFbLVd48/XkbYGRO0PgI/AAAAAAAAAL8/HZCSnBi5lQQ3Rp2GMgyVnjhMKTr2xQutwCLcBGAsYHQ/s1600/android-icon.png",
+    "sizes": "168x168",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-PcmCFbLVd48/XkbYGRO0PgI/AAAAAAAAAL8/HZCSnBi5lQQ3Rp2GMgyVnjhMKTr2xQutwCLcBGAsYHQ/s1600/android-icon.png",
+    "sizes": "192x192",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-PcmCFbLVd48/XkbYGRO0PgI/AAAAAAAAAL8/HZCSnBi5lQQ3Rp2GMgyVnjhMKTr2xQutwCLcBGAsYHQ/s1600/android-icon.png",
+    "sizes": "512x512",
+    "type": "image/png"
+  }]
+}
